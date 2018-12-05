474e16c275adbd5f4aca7bcb50d88ae292dd88a2
Ronny Moura <ronimour@gmail.com>
2018-12-05 20:17:24 -0200

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -97,13 +97,22 @@ app.post('/users', (req, res) => {
   });
 });
 
+app.get('/users/me', authenticate , (req, res) => {
+  res.send(req.user);
+});
+
 app.post('/users/login', logon, (req, res) => {
   res.header('x-auth', req.token).send(req.user);
 });
 
-app.get('/users/me', authenticate , (req, res) => {
-  res.send(req.user);
+app.delete('/users/me/token', authenticate , (req, res) => {
+  req.user.removeToken(req.token).then(()=>{
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
 });
+
 
 app.listen(port, () => {
   console.log(`Started up on port ${port}`);
