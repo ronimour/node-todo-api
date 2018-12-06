9219b402abe67c0ed92f71364a9e40dab78d3e9a
Ronny Moura <ronimour@gmail.com>
2018-12-05 23:06:33 -0200

    Make the routes private

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -20,18 +20,24 @@ const users = [{
   }, {
   _id: userTwoId,
   email: 'userTwo@example.com',
-  password: 'userTwoPass'
+  password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+    }]
   }
 ];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 },{
   _id: new ObjectID(),
   text: 'Second test todo',
   completed: true,
-  completedAt: 666
+  completedAt: 666,
+  _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
